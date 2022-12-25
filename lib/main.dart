bf0db4b0656c3f14c5526539a73e770e34a84e16
irkhamnazmi <irkham.nazmi@gmail.com>
2022-12-25 18:07:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Helo, Semuanya',
+          style: GoogleFonts.poppins(fontSize: 50),
+        ),
+      ),
+    );
+  }
+}
