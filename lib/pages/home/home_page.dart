c683d9ca3490249460cdcce5d6cdf377a7e20f9f
irkhamnazmi <irkham.nazmi@gmail.com>
2022-12-28 09:07:10 +0700

    add main page to create sub page home, chat, wishlist, profile and untill categories menu in home page

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Alex',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                  ),
+                  Text(
+                    '@alexkeinn',
+                    style: subtitleTextStyle.copyWith(
+                        fontSize: 16, fontWeight: regular),
+                  )
+                ],
+              ),
+            ),
+            Container(
+                width: 54,
+                height: 54,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/image_profile.png')),
+                )),
+          ],
+        ),
+      );
+    }
+
+    Widget categories() {
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(children: [
+            SizedBox(
+              width: defaultMargin,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12), color: primaryColor),
+              child: Text(
+                'All Shoes',
+                style:
+                    primaryTextStyle.copyWith(fontSize: 12, fontWeight: medium),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: subtitleColor),
+                  color: transparentColor),
+              child: Text(
+                'Running',
+                style: subtitleTextStyle.copyWith(
+                    fontSize: 12, fontWeight: medium),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: subtitleColor),
+                  color: transparentColor),
+              child: Text(
+                'Training',
+                style: subtitleTextStyle.copyWith(
+                    fontSize: 12, fontWeight: medium),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: subtitleColor),
+                  color: transparentColor),
+              child: Text(
+                'Basketball',
+                style: subtitleTextStyle.copyWith(
+                    fontSize: 12, fontWeight: medium),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: subtitleColor),
+                  color: transparentColor),
+              child: Text(
+                'Hiking',
+                style: subtitleTextStyle.copyWith(
+                    fontSize: 12, fontWeight: medium),
+              ),
+            ),
+          ]),
+        ),
+      );
+    }
+
+    return ListView(
+      children: [header(), categories()],
+    );
+  }
+}
